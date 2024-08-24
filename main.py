2702eaf81d3dd1b2bb63fed31e311abfdbcf02da
AdityaSinha2305 <sinhaaditya43@gmail.com>
2024-08-25 01:30:34 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,274 @@
+# Bakery Management System
+# Customer : Name , order_date , order_name ,quantity
+# Add an item , Update an item -> Quantity
+
+
+# Things to correct 
+'''
+1. If user enter string for numbers
+2. Check for valid data input in add_item and in update
+'''
+
+
+# Menu driven program
+
+import pandas as pd
+import datetime
+import os
+
+
+def add_item():
+    ch = input("Press 1 to continue or Press 0 to go back to main menu : ")
+
+    if ch.isnumeric():
+        if int(ch)==1:
+
+            # This while part is added because if user give invalid data for name,order_name and quantity then it will ask again for details until user give correct input
+            while True:
+                name = input("Enter customer name : ")
+                order_dt = datetime.datetime.now()
+                order_name = input("Enter your order name : ")
+                quantity = input('Enter the qunatity of order customer want : ')
+                
+                if name.isalpha() and order_name.isalpha() and quantity.isnumeric():
+                    data = {
+                        'Name' : name,
+                        'Order_name' : order_name,
+                        'Quantity' : quantity,
+                        'Order_date' : order_dt
+                    }
+
+                    # once data is valid come out of loop
+                    break
+
+                else:
+                    print()
+                    print("Enter valid data for each input.")
+                    print("----------------------------------")
+                    print()
+
+            print("1 Item added ✅")
+            print("----------------------------------")
+            return data
+        
+        elif int(ch)==0:
+            os.system('cls')
+            base(df)
+        
+        else:
+            # This part is for if ch is other than 0 or 1
+            print()
+            print("Select either 1 or 0")
+            print("---------------------------------------")
+            add_item()
+
+    else:
+        # This part is for ch is other than integer
+        print("Select your choice from above two options")
+        print("----------------------------------------")
+        print()
+        add_item()
+
+
+
+
+def update(df):
+    if df.empty:
+        print("No item inserted yet😕\nAdd item first")
+    else:
+        ch = input("Press 1 to continue or Press 0 to go back to main menu : ")
+        print()
+
+        if ch.isnumeric():
+            ch = int(ch)
+            if ch==1:
+
+                # This while shows options until user won't exit from the update menu
+                while(True):
+                    update_choice = input('''1. Press 1 to see the entire order details (so that you can't update wrong order 😅) \
+                            \n2. Press 2 to see a particular order details \
+                            \n3. Press 3 to update the required order (if you remembered the order_id 😎) \
+                            \n4. Press 4 to exit the update menu \n \
+                            \nSelect your choice from above option : ''')
+                    
+                    # This if part is for checking whether update_choice is integer or not
+                    if update_choice.isnumeric():
+                        update_choice = int(update_choice)
+                        if update_choice==1:
+                            display(df)
+                            print()
+                            print("------------------------------------------------")
+                            print()
+
+                        elif update_choice==2:
+                            # This while loop runs until order_id is not valid
+                            while True:
+                                print()
+                                order_id = input("Enter the order_id you want to see detail : ")
+                                if order_id.isnumeric():
+                                    order_id = int(order_id)
+                                    if order_id in list(df.index):
+                                        print(df.iloc[[order_id]])
+                                        print("----------------------------------------------------")
+                                        print()
+                                    else:
+                                        print()
+                                        print("Order id doesn't exist in the database")
+                                        print("-----------------------------------------")
+                                        print()
+                                    break
+                                else:
+                                    print()
+                                    print("Enter a valid order_id")
+                                    print()
+
+                            
+                        elif update_choice==3:
+                            print()
+
+                            while True:
+                                index = input("Enter the order id you want to update : ")
+                                col = input("Enter the column you want to update : ")         
+                                new_col_value = input("Enter the new quantity value : ")
+
+                                # This code is build for updating only quantity column as of now and not able to handle name column because new_col_value accepts only inetger value as of now
+                                # This part checks wheather each input is valid or not i.e index -> integer , col -> alphabets , new_col_value -> integer
+                                if index.isnumeric() and col.isalpha() and new_col_value.isnumeric():
+
+                                    index = int(index)
+
+                                    if index in list(df.index) and col in list(df.columns):
+                                        df.loc[index , col] = new_col_value
+                                        print()
+                                        print(f"{col} updated successfully ✅")
+                                        print("------------------------------------")
+                                        print()
+
+                                    elif index not in list(df.index):
+                                        print(f"Order id {index} doesn't exists")
+                                        print("------------------------------------")
+                                        print()
+
+                                    else:
+                                        print(f"{col} column doesn't exists")
+                                        print("-----------------------------------")
+                                        print()
+                                    
+                                    break
+
+                                else:
+                                    print()
+                                    print("Enter the valid data for each input.")
+                                    print("---------------------------------------")
+                                    print()
+                        
+                        elif update_choice==4:
+                            print("-----------------------------------------------------")
+                            break
+
+                        else:
+                            print()
+                            print("Select from above four options only")
+                            print("------------------------------------------")
+                            print()
+                    
+                    else:
+                        # This part is for for update_choice is alphabets or alphanumeric input
+                        print("Choose the valid options")
+                        print()
+
+            elif ch==0:
+                os.system('cls')
+                base(df)
+
+            else:
+                # This part is for if ch is other than 0 or 1
+                print()
+                print("Select either 1 or 0")
+                print("----------------------------------------")
+                update(df)
+
+        else:
+            # This part is for if ch is other than integer
+            print("Select your choice from above two options")
+            print("----------------------------------------")
+            print()
+            update(df)
+
+
+
+def display(df):
+    if df.empty:
+        print("Nothing to display yet..add item first")
+    else:    
+        print(df)
+
+
+
+def base(df):
+
+    while True:
+        choice = input('''Welcome to Bakery Management System🍔🍟🎂 \
+                           \n1. Add an item \
+                           \n2. Update an item \
+                           \n3. Display order list \
+                           \n4. Export data to excel sheet \
+                           \n5. Clear the screen \
+                           \n6. Exit \
+                           \nSelect the option number : ''')
+        
+        # This part is to check whether choice is integer or not
+        if choice.isnumeric():
+            choice = int(choice)
+            if choice==1:
+                os.system('cls')
+                new_df = add_item()
+                df = df._append(new_df, ignore_index=True)
+                print()
+
+            elif choice==2:
+                os.system('cls')
+                update(df)
+                print()
+
+            elif choice==3:
+                os.system('cls')
+                display(df)
+                print()
+                print("-------------------------------------")
+                print()
+
+            elif choice==4:
+                os.system('cls')
+                if df.empty:
+                    print("Buddy insert item first then export your data 😄")
+                    print("-----------------------------------------------------")
+                    print()
+                else:
+                    os.system('cls')
+                    df.to_csv('Customer.csv')
+                    print("Data exported to excel file succesfully ✅")
+                    print("-----------------------------------------------")
+                    print()
+
+            elif choice==5:
+                os.system('cls')
+
+            elif choice==6:
+                os.system('cls')
+                print("Happy Earning😉")
+                exit()
+            else:
+                print()
+                print("Select from above six options only")
+                print("------------------------------------------")
+                print()
+
+        else:
+            print()
+            print("Select from above options only.")
+            print("--------------------------------------")
+            print()
+
+df = pd.DataFrame()
+base(df)
